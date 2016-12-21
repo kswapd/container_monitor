efeaa0444325a5f078f82a7b587ca354296c98e9
TangQian <qian.tang@hnair.com>
2016-12-21 10:10:30 +0800

    config

--- script/build.sh
+++ script/build.sh
@@ -1,2 +1,2 @@
 docker run --rm -v "$PWD":/go/src/monitor/container_monitor -w /go/src/monitor/container_monitor golang:1.7.3 go build -v -o bin/container_monitor
-docker build -t mutemaniac/container-monitor:v0.6.2 .
+docker build -t mutemaniac/container-monitor:latest .
